44101391db4fdde3d4b1e197183821c89e030bed
Nux007 <007.nux@gmail.com>
2021-04-05 08:10:30 +0200

    Exo 207.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 1. Créez un dossier Entity
+ * 2. Créez un dossier Manager ou mieux Repository
+ * 3. Dans le dossier Entity, créez trois classes User, Role et Article, ces classes doivent avoir un constructeur vide
+ * 4. Dans le dossier Manager ( ou Repository ), créez les classes User, Role et Article.
+ * 5. Les trois classes doivent avoir une méthode save qui prendra en paramètre l'objet qui est géré par la classe
+ *    p.ex : User $user ( Entity ) pour la class User ( Manager )
+ *    Aucun code ne vous est demandé mis à part la déclaration des namespaces et les use, les méthodes peuvent donc être vides.
+ * 6. Utilisez au maximum les namespaces et les Use !!!
+ *
+ * Poussez votre code sur Github et envoyez moi le lien !
+ */
